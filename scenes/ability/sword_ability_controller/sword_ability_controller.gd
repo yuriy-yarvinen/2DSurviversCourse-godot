39b916bc805d777234f45d0a7c6a5bd2d64ec598
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2023-04-16 18:09:19 +0300

    add that sword hit enemy

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -21,7 +21,7 @@ func on_timer_timeout():
 	enemies = enemies.filter(func(enemy: Node2D):
 		return enemy.global_position.distance_squared_to(player.global_position) < pow(max_range, 2)
 	)
-	if enemies.size() < 0:
+	if enemies.size() == 0:
 		return
 	
 	enemies.sort_custom(func(a: Node2D, b: Node2D):
@@ -33,3 +33,7 @@ func on_timer_timeout():
 	var sword_instance = sword_ability.instantiate() as Node2D
 	player.get_parent().add_child(sword_instance)
 	sword_instance.global_position = enemies[0].global_position
+	sword_instance.global_position += Vector2.RIGHT.rotated(randf_range(0, TAU)) * 4
+
+	var enemy_direction = enemies[0].global_position - sword_instance.global_position
+	sword_instance.rotation = enemy_direction.angle()
