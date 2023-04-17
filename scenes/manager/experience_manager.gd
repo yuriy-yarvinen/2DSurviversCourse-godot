eef6b8a06e0ecba5da05bbe6b5306df4bb8ab96e
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2023-04-17 23:12:00 +0300

    add gameevents

---
+++ scenes/manager/experience_manager.gd
@@ -0,0 +1,12 @@
+extends Node
+
+var current_experience = 0
+
+func _ready():
+	GameEvents.experience_vial_collected.connect(on_experience_vial_collected)
+
+func increment_experience(number: float):
+	current_experience += number
+
+func on_experience_vial_collected(number: float):
+	increment_experience(number)
