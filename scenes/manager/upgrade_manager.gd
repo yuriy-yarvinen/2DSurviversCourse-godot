d6ef536c8563299546462c8e7a3b71af58fe31c3
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2023-04-18 23:55:35 +0300

    enabling upgrade selection

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -3,24 +3,35 @@ extends Node
 
 @export var upgrade_pool: Array[AbilityUpgrade]
 @export var experience_manager: Node
+@export var upgrade_screen_scene: PackedScene
 
 var current_upgrades = {}
 
 func _ready():
 	experience_manager.level_up.connect(on_level_up)
 	
-
+	
 func on_level_up(current_level: int):
 	var chosen_upgrade = upgrade_pool.pick_random()
 	if chosen_upgrade == null:
 		return
+	var upgrade_scene_instance = upgrade_screen_scene.instantiate()
+	add_child(upgrade_scene_instance)
+	upgrade_scene_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
+	upgrade_scene_instance.upgrade_selected.connect(on_upgrade_selected)
 	
-	var has_upgrade = current_upgrades.has(chosen_upgrade.id)
+	
+func apply_upgrade(upgrade: AbilityUpgrade):
+	
+	var has_upgrade = current_upgrades.has(upgrade.id)
 	if !has_upgrade:
-		current_upgrades[chosen_upgrade.id] = {
-			"resource": chosen_upgrade,
+		current_upgrades[upgrade.id] = {
+			"resource": upgrade,
 			"quantity": 1
 		}
 	else:
-		current_upgrades[chosen_upgrade.id]["quantity"] += 1
-	print(current_upgrades)
+		current_upgrades[upgrade.id]["quantity"] += 1
+	
+	
+func on_upgrade_selected(upgrade: AbilityUpgrade):
+	apply_upgrade(upgrade)
