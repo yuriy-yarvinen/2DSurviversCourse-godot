627a9c4a11d0b76d11fc17a2c9e54380cf64f260
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2023-04-20 07:19:26 +0300

    add victory screen

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,7 +1,18 @@
 extends Node
 
+@export var victory_screen: PackedScene
+
 @onready var timer = $Timer
+
+
+func _ready():
+	timer.timeout.connect(on_timer_timeout)
 
 
 func get_time_elapsed():
 	return timer.wait_time - timer.time_left
+
+
+func on_timer_timeout():
+	var victory_screen_istance = victory_screen.instantiate()
+	add_child(victory_screen_istance)
