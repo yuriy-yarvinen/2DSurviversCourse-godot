7661c235825475f249ff569752d104dd9d98b34d
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2023-04-23 23:26:20 +0300

    theme floating damage text

--- scenes/component/hurtbox_component.gd
+++ scenes/component/hurtbox_component.gd
@@ -3,6 +3,8 @@ class_name  HurtboxComponent
 
 @export var healt_component: Node
 
+var floating_text_scene = preload("res://scenes/ui/floating_text.tscn")
+
 func _ready():
 	area_entered.connect(on_area_entered)
 	
@@ -17,3 +19,7 @@ func on_area_entered(other_area: Area2D):
 	
 	var hitbox_component = other_area as HitboxComponent
 	healt_component.damage(hitbox_component.damage)
+	var floating_text_scene_instance = floating_text_scene.instantiate() as Node2D
+	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text_scene_instance)
+	floating_text_scene_instance.global_position = global_position
+	floating_text_scene_instance.start(str(hitbox_component.damage))
